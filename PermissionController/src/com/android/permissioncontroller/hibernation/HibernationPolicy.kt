b026f11de8d61d8ed288e804bbe6851e9a104d0e
Oliver Scott <olivercscott@gmail.com>
2021-11-08 22:28:34 -0500

    Enable permission autorevoke feature for work profiles
    Change-Id: I812f8c57308b0ebc6bf6d8edce5644fdc5cdc781

--- PermissionController/src/com/android/permissioncontroller/hibernation/HibernationPolicy.kt
+++ PermissionController/src/com/android/permissioncontroller/hibernation/HibernationPolicy.kt
@@ -598,11 +598,11 @@ suspend fun isPackageHibernationExemptBySystem(
         }
         return true
     }
-    if (Utils.isUserDisabledOrWorkProfile(user)) {
+    if (Utils.isUserDisabled(user)) {
         if (Log.isLoggable(LOG_TAG, Log.INFO)) {
             DumpableLog.i(
                 LOG_TAG,
-                "Exempted ${pkg.packageName} - $user is disabled or a work profile"
+                "Exempted ${pkg.packageName} - $user is disabled"
             )
         }
         return true
